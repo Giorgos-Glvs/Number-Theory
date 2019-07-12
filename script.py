f1f41d8ac67d011b10370c03ab56c2c5cbf41fbd
Giorgos-Glvs <ggal@baresquare.com>
2019-07-12 10:58:02 +0300

    User input

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 print("Lets explore the chaotic world of Numbers!")
+number = int(input("Give an integer number greater than zero: \n"))
+
+if number%2==0:
+    print("The number {} is even!".format(number))
+else:
+    print("The number {} is odd!".format(number))
