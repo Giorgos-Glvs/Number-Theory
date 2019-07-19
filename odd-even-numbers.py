3b30dd771b2128721cc7f80a711f488bf3902f3a
Giorgos-Glvs <ggal@baresquare.com>
2019-07-19 18:08:13 +0300

    Script renamed

---
+++ odd-even-numbers.py
@@ -0,0 +1,17 @@
+
+number = int(input("Give an integer number: \n"))
+
+while True:
+    if number<0:
+        number = int(input("The number should be greater than zero. Try again: \n"))
+    else:
+        break
+
+if number%2==0:
+    print("The number {} is even!".format(number))
+else:
+    print("The number {} is odd!".format(number))
+
+print("Have a nice day! Hope to see you again soon!")
+print("Bye darling!")
+print("Geia sou")
