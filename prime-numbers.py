1ee2a79aac520d625bb9d35a5593b1d0e3e41a2c
Giorgos-Glvs <ggal@baresquare.com>
2019-07-19 18:28:43 +0300

    Error fix

--- prime-numbers.py
+++ prime-numbers.py
@@ -3,8 +3,7 @@ number = int(input("Give an integer number greater than 1: \n"))
 
 while True:
     if number <1: 
-        number = int(input("The number should be an integer greater than 1. Try again: \n")
-    
+        number = int(input("The number should be an integer greater than 1. Try again: \n"))
     else:
         break
 print("The number you gave is: ".format(number))
