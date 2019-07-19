df622c6f9ad207f9f7c51866bf6035137bf4462e
Giorgos-Glvs <ggal@baresquare.com>
2019-07-19 18:23:25 +0300

    Prime numbers first

---
+++ prime-numbers.py
@@ -0,0 +1,11 @@
+
+number = int(input("Give an integer number greater than 1: \n"))
+
+while True:
+    if number <1: 
+        number = int(input("The number should be an integer greater than 1. Try again: \n")
+    
+    else:
+        break
+print("The number you gave is: ".format(number))
+print("See you soon!")
