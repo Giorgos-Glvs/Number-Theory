59940ee6bf5c0479937e45d9e17ab3e9c141e153
Giorgos-Glvs <49406024+Giorgos-Glvs@users.noreply.github.com>
2019-07-30 16:38:59 +0300

    Update prime-numbers.py

--- prime-numbers.py
+++ prime-numbers.py
@@ -20,5 +20,5 @@ else:
     print("The number you gave, {}, is not a prime number!".format(number))
 
 print("I hope you now you are wiser in Prime numbers!")
-print("This line was added through Git, from the command line!")
-print("Goodbye!")
+print("The previous line was deleted from GitHub!")
+print("To make a change, first fetch from here!")
